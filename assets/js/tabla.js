50574b7a8fe5ff211aed9268d2753c61bd1c7bd9
Jona Fernandez <jonaafernandez02@gmail.com>
2021-01-26 17:37:51 -0300

    actualizacion y correccion. falta progress bar que no funciona

--- assets/js/tabla.js
+++ assets/js/tabla.js
@@ -37,12 +37,11 @@ function iniciarPagina() { // carga pagina por completo
         }
     ]
 
-
-
-
-
+    /* Declaro variables de ambito global */
     let tbody = document.createElement("tbody");
     let tHeader = document.createElement("thead");
+    let btnConfirmarBorrar = document.getElementById("confirmarBorrar");
+    let btnConfirmarEditar = document.getElementById("confirmarEditar");
 
     function crearTablas(arr) {
         limpiartabla();
@@ -75,6 +74,7 @@ function iniciarPagina() { // carga pagina por completo
         fila.appendChild(casilleroTema);
         fila.appendChild(casilleroAcciones);
         tHeader.appendChild(fila);
+
         /* voy poniendo la tabla en la id "tablita" del html */
         document.getElementById("tablita").appendChild(tHeader);
 
@@ -85,22 +85,10 @@ function iniciarPagina() { // carga pagina por completo
             let apellido = document.createElement("td");
             let email = document.createElement("td");
             let tema = document.createElement("td");
+
             /* botones EDITAR Y ELIMINAR en cada fila,  */
             let btnBorrar = document.createElement("button");
             let btnEdit = document.createElement("button");
-            let borrarmodal = document.getElementById("borrarModal");
-            btnBorrar.innerText = "Eliminar";
-            btnEdit.innerText = "Editar";
-
-            // darle id a cada fila
-            fila.id = i;
-
-            /* listeners de botones para las funciones */
-            document.getElementById("agregar-Modal").addEventListener("click", agregar);
-            btnBorrar.addEventListener("click", e => eliminar(fila.id));
-            borrarmodal.addEventListener("click", e => eliminar(cargaEjemplo));
-            btnEdit.addEventListener("click", e => editar(fila.id));
-            btnEdit.addEventListener("click", editar)
 
             /* estilos para el cuerpo de la tabla */
             nombre.classList.add("bg-dark", "text-light", "p-2", "border-derecha", "border-izquierda", "border-abajo", "text-center");
@@ -110,7 +98,6 @@ function iniciarPagina() { // carga pagina por completo
             btnEdit.classList.add("bg-dark", "text-primary", "p-2", "border-derecha", "border-abajo", "text-center");
             btnBorrar.classList.add("bg-dark", "text-primary", "p-2", "border-derecha", "border-abajo", "text-center");
 
-            /*     btnBorrar.setAttribute(data - target, "#exampleModal2"); */
 
             /* aplico los inner text del arreglo de ejemplo para aplicarlos en la lista */
             nombre.innerText = arr[i].Nombre;
@@ -118,7 +105,32 @@ function iniciarPagina() { // carga pagina por completo
             email.innerText = arr[i].Email;
             tema.innerText = arr[i].Tema;
 
-            /* meto adentro de cada fila  */
+            btnBorrar.innerText = "Eliminar";
+            btnEdit.innerText = "Editar";
+
+            /* seteo atributos para vincularlos con los modals */
+            btnEdit.setAttribute('data-toggle', 'modal');
+            btnEdit.setAttribute('data-target', '#modalEditar');
+
+            btnBorrar.setAttribute('data-toggle', 'modal');
+            btnBorrar.setAttribute('data-target', '#modalBorrar');
+
+            // doy id a cada fila
+            fila.id = i;
+
+            /* Listeners de botones para las funciones */
+
+            btnBorrar.addEventListener("click", e => {
+                btnConfirmarBorrar.setAttribute('data-target', fila.id);
+            });
+
+            btnEdit.addEventListener("click", e => {
+                btnConfirmarEditar.setAttribute('data-target', fila.id);
+                console.log("ESTOY ACA EN LA FILA : " + fila.id);
+                cargaEditar(fila.id);
+            });
+
+            /*armado de cada fila de la tabla */
             fila.appendChild(nombre);
             fila.appendChild(apellido);
             fila.appendChild(email);
@@ -128,41 +140,75 @@ function iniciarPagina() { // carga pagina por completo
             tbody.appendChild(fila);
             document.getElementById("tablita").appendChild(tbody);
             /*  btnBorrar.addEventListener("click", eliminar); */
-
         }
-
     }
 
+    /* LISTENER boton confirmar modal */
+    btnConfirmarEditar.removeEventListener("click", (e) => confirmarEditar(parseInt(e.target.dataset.target)));
+    btnConfirmarEditar.addEventListener("click", (e) => confirmarEditar(parseInt(e.target.dataset.target)))
+    btnConfirmarBorrar.addEventListener("click", (e) => eliminar(btnConfirmarBorrar.dataset.target));
 
-    crearTablas(cargaEjemplo); //CARGO LA TABLA
+    /* CARGO LA TABLA */
+    crearTablas(cargaEjemplo);
+
+    /* FUNCIONES */
+
+    document.getElementById("agregar-Modal").addEventListener("click", agregar);
 
     function agregar() {
         let nombre = document.getElementById("nombreModal");
         let apellido = document.getElementById("apellidoModal");
         let email = document.getElementById("emailModal");
         let tema = document.getElementById("temaModal");
-        console.log(nombre);
+        console.log(nombre.value);
         let ingreso = {
             "Nombre": nombre.value,
             "Apellido": apellido.value,
             "Email": email.value,
             "Tema": tema.value
         }
+
+        document.getElementById("nombreModal").value = "";
+        document.getElementById("apellidoModal").value = "";
+        document.getElementById("emailModal").value = "";
+        document.getElementById("temaModal").value = "";
+
         cargaEjemplo.push(ingreso);
         limpiartabla();
         crearTablas(cargaEjemplo);
+
+        /*   btnEdit.removeEventListener("click", (e) => cargaEditar(parseInt(e.target.dataset.target)));
+          btnEdit.addEventListener("click", (e) => cargaEditar(parseInt(e.target.dataset.target))); */
+
     }
 
-    function editar(pos) {
-        let nombre = document.getElementById("nombreModal");
-        let apellido = document.getElementById("apellidoModal");
-        let email = document.getElementById("emailModal");
-        let tema = document.getElementById("temaModal");
-        /*         console.log(nombre.value[pos]); */
+    function cargaEditar(pos) {
+        console.log("ENTRE A CARGAR EDITAR ! ! ");
+        let nombre = document.getElementById("nombreModalEditar");
+        let apellido = document.getElementById("apellidoModalEditar");
+        let email = document.getElementById("emailModalEditar");
+        let tema = document.getElementById("temaModalEditar");
+
         nombre.value = cargaEjemplo[pos].Nombre;
         apellido.value = cargaEjemplo[pos].Apellido;
         email.value = cargaEjemplo[pos].Email;
         tema.value = cargaEjemplo[pos].Tema;
+        limpiartabla();
+        crearTablas(cargaEjemplo);
+    }
+
+    function confirmarEditar(pos) {
+        console.log("ENTRO EN CONFIRMAR EDITAR!!");
+        let nombre = document.getElementById("nombreModalEditar");
+        let apellido = document.getElementById("apellidoModalEditar");
+        let email = document.getElementById("emailModalEditar");
+        let tema = document.getElementById("temaModalEditar");
+
+
+        cargaEjemplo[pos].Nombre = nombre.value;
+        cargaEjemplo[pos].Apellido = apellido.value;
+        cargaEjemplo[pos].Email = email.value;
+        cargaEjemplo[pos].Tema = tema.value;
 
         limpiartabla();
         crearTablas(cargaEjemplo);
@@ -175,7 +221,6 @@ function iniciarPagina() { // carga pagina por completo
             cargaEjemplo.pop();
         } else {
             cargaEjemplo.splice(pos, 1);
-
         }
         limpiartabla();
         crearTablas(cargaEjemplo);
@@ -187,14 +232,17 @@ function iniciarPagina() { // carga pagina por completo
 
     }
 
+    let barritaProgress = document.getElementById("modalBarrita");
+    barritaProgress
 
 
-
-
-
-
-
-
+    /* function cargarBarrita() {
+        let progressBar = document.getElementById("barritaModal");
+        progressBar.setAttribute("aria-valuenow", 0);
+        progressBar.style.width = `0%`;
+        console.log("PUTO");
+    }
+    cargarBarrita(); */
 } //iniciar pagina
 
 document.addEventListener("DOMContentLoaded", iniciarPagina);
